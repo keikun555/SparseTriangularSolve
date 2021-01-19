62d2ae259f420ed86265ebe19473ead111626174
Kei Imada <kimada1@swarthmore.edu>
2021-01-18 16:17:49 -0800

    improved level partitioning speed

--- include/partition.h
+++ include/partition.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <omp.h>
 #include <vector>
 
 #include "adjacencyListGraph.h"
@@ -26,74 +27,61 @@ template <typename T>
 void Partition::from_lower_triangular_matrix(CCSMatrix<T> *matrix) {
   AdjacencyListGraph<int, int, int>
       dependency_graph; // node matrix col/x elt edge which col it depends on
+  STLHashTable<int, int> num_parents_dict;
+  STLHashTable<int, bool> orphans;
   for (int j = 0; j < matrix->num_col_get(); j++) {
     // populate nodes
     dependency_graph.insertVertex(j);
+    num_parents_dict.insert(j, 0);
+    orphans.insert(j, true);
   }
   for (int j = 0; j < matrix->num_col_get(); j++) {
     // for every column
-    for (int p = matrix->column_pointer_get()[j] + 1;
+    for (int p = matrix->column_pointer_get()[j];
          p < matrix->column_pointer_get()[j + 1]; p++) {
       // for every elt in the column
-      if (matrix->values_get()[p] != 0) {
-        // if elt is nonzero, the columns in the row are dependent on current
+      int row_idx = matrix->row_index_get()[p];
+      if (row_idx != j && matrix->values_get()[p] != 0) {
+        // if elt is nonzero the corresponding column is dependent on current
         // col
-        for (int depending_col = j + 1; // next column
-             depending_col <
-             matrix->row_index_get()[p] + 1; // include diagonal of row
-             depending_col++) {
-          // until we hit the diagonal
-          if (!dependency_graph.containsEdge(j, depending_col)) {
-            dependency_graph.insertEdge(j, depending_col, 0, 0);
-          }
+        // #pragma omp critical
+        dependency_graph.insertEdge(j, row_idx, 0, 0);
+        num_parents_dict.update(row_idx, num_parents_dict.get(row_idx) + 1);
+        if (orphans.contains(row_idx)) {
+          orphans.remove(row_idx);
         }
+        break;
       }
     }
   }
+  cout << "dependency graph populated" << endl;
   // level partitioning
   unsigned int num_partitioned =
       0; // counter to check for circular dependencies
-  STLHashTable<int, int> num_neighbors_dict;
-  vector<int> partition, new_partition;
-  for (unsigned int i = 0; i < dependency_graph.getVertices().size(); i++) {
-    // make first partition
-    int v = dependency_graph.getVertices()[i];
-    vector<Edge<int, int, int>> incoming_edges =
-        dependency_graph.getIncomingEdges(v);
-    num_neighbors_dict.insert(v, incoming_edges.size());
-    if (incoming_edges.size() == 0) {
-      partition.push_back(v);
-      num_partitioned++;
-    }
-  }
-  if (partition.size() > 0) {
-    partitioning.push_back(partition);
-  }
-  while (partition.size() > 0) {
-    vector<int> new_partition;
+  while (orphans.getSize() > 0) {
+    vector<int> partition = orphans.getKeys();
     for (unsigned int i = 0; i < partition.size(); i++) {
-      int v = partition.at(i);
+      int v = partition[i];
+      num_partitioned++;
+      orphans.remove(v);
       vector<Edge<int, int, int>> outgoing_edges =
           dependency_graph.getOutgoingEdges(v);
       for (unsigned int j = 0; j < outgoing_edges.size(); j++) {
-        int v = outgoing_edges[j].target;
-        int new_num_neighbors = num_neighbors_dict.get(v) - 1;
-        num_neighbors_dict.update(v, new_num_neighbors);
-        if (new_num_neighbors == 0) {
-          new_partition.push_back(v);
-          num_partitioned++;
+        int child = outgoing_edges[j].target;
+        int new_num_parent = num_parents_dict.get(child) - 1;
+        num_parents_dict.update(child, new_num_parent);
+        if (new_num_parent <= 0) {
+          orphans.insert(child, true);
         }
       }
     }
-    if (new_partition.size() > 0) {
-      partitioning.push_back(new_partition);
-    }
-    partition = new_partition;
+    partitioning.push_back(partition);
   }
   // if we reached here with num_partitioned < num_vertices, we have a circular
   // dependency
   if (num_partitioned < dependency_graph.getVertices().size()) {
-    throw runtime_error("Circular dependency found during the partitioning");
+    throw runtime_error("Circular dependency found during partitioning, is the "
+                        "matrix really lower triangular?");
   }
 }
 
